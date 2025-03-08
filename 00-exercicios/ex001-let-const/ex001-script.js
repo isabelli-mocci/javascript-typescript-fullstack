8d3b911ce342ab20253cfc5f188fd26d1e27f840
Isabelli Mocci <isabellimocci.tech@gmail.com>
2025-03-08 19:32:33 -0300

    feat: practice using const, let with IMC calculation
    - declared variables for name, surname, age, weight, and height
    - calculated IMC using the formula (weight / height^2)
    - displayed the results in a formatted string in the console
    - calculated and displayed birth year based on current age

--- 00-exercicios/ex001-let-const/ex001-script.js
+++ 00-exercicios/ex001-let-const/ex001-script.js
@@ -1,15 +1,22 @@
 // =============================================================
 // Curso de JavaScript e TypeScript
 // Profº Luiz Otávio Miranda
-// Ex002 - Usando Const e Let
+// Ex001 - Usando Const e Let
 // Isabelli Cristina Mocci - 05/03/2025
 // =============================================================
 
 /*
 Objetivo: 
 
-Isabelli Cristina Mocci tem 25 anos, pesa 74kg, tem 1.55m de altura e seu IMC é de 30.80124869927159.
-Isabelli nasceu em 1999.
+Praticar a declaração de variáveis, operações matemáticas e concatenação de strings no JavaScript.
+
+Instruções: 
+1. Exiba no console uma mensagem com as informações formatadas, seguindo este modelo: 
+  "Isabelli Cristina Mocci tem 25 anos, pesa 74kg, tem 1.55m de altura e seu IMC é de 30.80124869927159."
+  "Isabelli nasceu em 1999."
+
+ 2.  Calcule o IMC (Índice de Massa Corporal) usando a fórmula:  
+  IMC = peso / (altura * altura)
 */
 // =============================================================
 
