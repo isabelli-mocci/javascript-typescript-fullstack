997f94191a6f610847e83b4f85d3c4c40cd38505
Isabelli Mocci <isabellimocci.tech@gmail.com>
2025-03-08 00:26:27 -0300

    feat: add all exercises from JavaScript Basics module
    Includes all exercises from module 1 covering variables, functions, and basic control structures.

---
+++ 01-javascript-basico/aula001-const-let.js
@@ -0,0 +1,32 @@
+// =============================================================
+// Curso de JavaScript e TypeScript
+// Profº Luiz Otávio Miranda
+// Ex001 - Usando Const e Let
+// Isabelli Cristina Mocci - 05/03/2025
+// =============================================================
+
+/*
+Objetivo: 
+
+Isabelli Cristina Mocci tem 25 anos, pesa 74kg, tem 1.55m de altura e seu IMC é de 30.80124869927159.
+Isabelli nasceu em 1999.
+*/
+// =============================================================
+
+const nome = "Isabelli Cristina";
+const sobrenome = "Mocci";
+const idade = 26;
+const peso = 74;
+const alturaEmMetro = 1.55;
+
+let IMC; // peso / (altura * altura)
+IMC = peso / (alturaEmMetro * alturaEmMetro);
+
+let anoNascimento;
+anoNascimento = 2025 - idade;
+
+console.log(
+  `${nome} ${sobrenome} tem ${idade} anos, pesa ${peso}kg, tem ${alturaEmMetro}m de altura e seu IMC é de ${IMC}.`
+);
+
+console.log(`${nome} nasceu em ${anoNascimento}.`);
